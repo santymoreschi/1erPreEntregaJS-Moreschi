f685944d5c0701b03cfac143120483a9188cc773
santymoreschi <santycuarto@gmail.com>
2023-07-09 19:03:43 -0300

    pre-entrega1

--- js/ticket.js
+++ js/ticket.js
@@ -1,123 +1,39 @@
-
-
-/*function saludar(){
-    console.log("hola alumnos")
-    console.log("¿como va con js ?")
-    console.log("espero que de 10")
-
-}*/
-
-
-
-//acumulador de productos
-let resultado = 0
-let ticket = "Detalle de la Compra:"
-
-let rta = ""
-
-do{
-    let producto = prompt("ingrese el nombre del producto:")
-    let precio = Number(prompt("ingrese el valor del producto: "))
-    
-    resultado = resultado + precio
-    ticket = ticket + "\n" + Producto + "\t$" + precio
-  
-    rta = prompt("desea salir?(ingrese 'salir' para salir.)")
-
-}while(rta != "salir")
-
-alert(ticket + "\nTotal: "+resultado)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let uncolor = "Rojo"
-
-
-con (uncolor == "rojo") comparamos si uncolor es igual "Rojo"
-
-if (uncolor == "Rojo") {
-    console.log("el rojo es Rojo");
-}else {
-    la instruccion se interpreta cuando unColor no es "Rojo" 
-      console.log("el color NO es Rojo");
+//Saludo
+function saludar() {
+    alert("Gracias por realizar tu compra");
+    alert("A continuación te pediremos que ingreses los productos que compraste");
 }
+saludar();
 
 
-var numero = 10;
+//Acumulador de productos
+let resultado = 0;
+let ticket = "Detalle de la Compra:";
+let rta = "";
 
-if (numero < 11) {
-  alert("se ejecutaron las instrucciones");
-}else {
-    //instrucciones
-    alert("no se ejecutaron las instrucciones");
+do {
+let producto = prompt("ingrese el nombre del producto:");
+let precio = Number(prompt("ingrese el valor del producto: "))
+
+
+if (producto && precio && !isNaN(precio)) {
+resultado += precio;
+ticket += "\n" + producto + "\t$" + precio.toFixed(2);
+rta = prompt("¿Desea salir? (Ingrese 'SI' para salir.)").toUpperCase();
+} else {
+alert("Ingrese valores válidos para el nombre y el precio del producto.");
 }
+} while (rta !== "SI");
+alert(ticket + "\nTotal: $" + resultado.toFixed(2));
 
 
 
 
-  CLASE NUMERO 3
+//¿Eres mayor de edad?
+let edad = 20;
 
-let entrada = ""
-
-
-REPETIMIS CON while hasta que el usuario ingtrsa "ESC"
-while (condicion) {
-
-    
-        1 le pregunto el nombre de usuario 
-        2 lo muestro
-        3 desea continuar cargando usuarios?
-    
+if (edad >= 18) {
+console.log("Eres mayor de edad");
+} else {
+console.log("Eres menor de edad");
 }
-
-let rta = "ESC"
-
-while (rta = "ESC") {
-
-    1 le pregunto el nombre de
-    let nombre = prompt("¿cual es si nombre?")
-
-
-    2 le pregunto su apellido
-    let apellido = prompt("¿cual es su apellido?")
-
-    3 notificar al usuario 
-    alert("los datos fueron cargados con exitos")
-    alert("los datos guardados son \nNombre : " + nombre + "\nApellido: " + apellido)
-
-}
-
-
-
-let opcion = 2;
-
-switch (opcion) {
-    case 1:
-        console.log("La opción seleccionada es 1");
-      break;
-    case 2:
-        console.log("La opción seleccionada es 2");
-        break;
-    case 3:
-        console.log("La opción seleccionada es 3");
-        break;
-    default:
-        console.log("La opción seleccionada no es válida");
-//} 
-
-*/
-
-
-
