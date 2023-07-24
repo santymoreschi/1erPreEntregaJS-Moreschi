b00702d2e826df2fd4ce7b3bc81ef07f1df9472c
santymoreschi <santycuarto@gmail.com>
2023-07-23 23:18:04 -0300

    segundaPreEntrega2

--- js/ticket.js
+++ js/ticket.js
@@ -1,3 +1,5 @@
+/*
+
 //Saludo
 function saludar() {
     alert("Gracias por realizar tu compra");
@@ -36,4 +38,112 @@ if (edad >= 18) {
 console.log("Eres mayor de edad");
 } else {
 console.log("Eres menor de edad");
+} 
+*/
+
+/*
+//length
+const listanumerica = (10, "hola", true, 44, -10, 20, 30, 40, 50, "ho", "la");
+
+for (let i = 0; i < listanumerica.length; i++) {
+    console.log(listanumerica[i])
 }
+
+
+
+function calcularArea() {
+    const radio = parseFloat(document.getElementById('radio').value);
+
+    if (isNaN(radio)) {
+        document.getElementById('resultado').innerText = "Ingresa un número válido.";
+        return;
+    }
+
+    const area = Math.PI * Math.pow(radio, 2);
+    document.getElementById('resultado').innerText = `El área del círculo es: ${area.toFixed(2)}`;
+}
+
+// Saludo
+function saludar() {
+    alert("Gracias por realizar tu compra");
+    alert("A continuación te pediremos que ingreses los productos que compraste");
+}
+saludar();
+
+// Acumulador de productos
+let resultado = 0;
+let ticket = "Detalle de la Compra:";
+let rta = "";
+
+do {
+    let producto = prompt("Ingrese el nombre del producto:");
+    let precio = parseFloat(prompt("Ingrese el valor del producto: "));
+
+    if (producto && !isNaN(precio)) {
+        resultado += precio;
+        ticket += "\n" + producto + "\t$" + precio.toFixed(2);
+        rta = prompt("¿Desea salir? (Ingrese 'SI' para salir.)").toUpperCase();
+    } else {
+        alert("Ingrese valores válidos para el nombre y el precio del producto.");
+    }
+} while (rta !== "SI");
+document.write(ticket + "<br>Total: $" + resultado.toFixed(2));
+
+// ¿Eres mayor de edad?
+let edad = 20;
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
+*/
+
+
+
+// Datos de los productos disponibles en la tienda
+const products = [
+    { id: 1, name: 'Smartphone', category: 'Electrónicos', price: 300, image: 'img/smartphone.jpg' },
+    { id: 2, name: 'Smart TV', category: 'Electrónicos', price: 800, image: 'img/smart.jpg' },
+    { id: 3, name: 'Laptop', category: 'Electrónicos', price: 1200, image: 'img/laptop.jpg' },
+    { id: 4, name: 'Tablet', category: 'Electrónicos', price: 250, image: 'img/tablet.jpg' },
+    { id: 5, name: 'Auriculares', category: 'Electrónicos', price: 50, image: 'img/auriculares.jpg' },
+];
+
+// Variable para almacenar los productos filtrados
+let filteredProducts = products.slice(); // Creamos una copia del array original
+
+// Función para mostrar los productos en la página
+function showProducts() {
+    const productListDiv = document.getElementById('productList');
+    productListDiv.innerHTML = '';
+
+    filteredProducts.forEach((product) => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" style="height: 150px; object-fit: cover;">
+            <h3>${product.name}</h3>
+            <p>Categoría: ${product.category}</p>
+            <p>Precio: $${product.price}</p>
+        `;
+
+        productListDiv.appendChild(productCard);
+    });
+}
+
+// Función para filtrar productos por nombre
+function filterProducts() {
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm)
+    );
+
+    showProducts();
+}
+
+// Mostrar los productos al cargar la página
+showProducts();
