0990022503759add0d0643cc439c60dc98f9e161
santymoreschi <santycuarto@gmail.com>
2023-07-26 20:11:33 -0300

    segunda pre entrega

--- js/carrito.js
+++ js/carrito.js
@@ -23,3 +23,14 @@ alert("Ingrese valores válidos para el nombre y el precio del producto.");
 }
 } while (rta !== "SI");
 alert(ticket + "\nTotal: $" + resultado.toFixed(2));
+
+
+
+/*//Saludo
+function saludar() {
+alert("Bienvenido a Games Market");
+alert("A continuación te pediremos que ingreses algunos datos para poder darte una experiencia grandiosa");
+alert("Recuerda que toda esta informacion es 100% confidencial y con objetivos de brindar la mejor asistencia para su comodidad");
+alert("Recuerda tambien tomar nota de las compras que incluyas en el carrito para que puedas asegurar los gastos que estas por realizar");
+}
+saludar();*/
--- js/ticket.js
+++ js/ticket.js
@@ -1,12 +1,35 @@
-
-//Saludo
+// Función para saludar al cliente y solicitar nombre, edad y localidad
 function saludar() {
-    alert("Bienvenido a Games Market");
-    alert("A continuación te pediremos que ingreses algunos datos para poder darte una experiencia grandiosa");
-    alert("Recuerda que toda esta informacion es 100% confidencial y con objetivos de brindar la mejor asistencia para su comodidad");
-    alert("Recuerda tambien tomar nota de las compras que incluyas en el carrito para que puedas asegurar los gastos que estas por realizar");
+var nombre = prompt("Por favor, ingresa tu nombre:");
+var apellido = prompt("Por favor, ingresa tu apellido:");
+
+if (nombre === null || nombre === "") {
+    alert("No ingresaste un nombre válido.");
+} else if (apellido === null || apellido === "") {
+    alert("No ingresaste un apellido válido.");
+} else {
+    alert("¡Hola, " + nombre + " " + apellido + "! Bienvenido/a a Games Market.");
+    var edad = prompt("Por favor, ingresa tu edad:");
+    if (edad === null || edad === "" || isNaN(edad)) {
+        alert("No ingresaste una edad válida.");
+    } else {
+        var localidad = prompt("Por favor, ingresa tu localidad:");
+        if (localidad === null || localidad === "") {
+            alert("No ingresaste una localidad válida.");
+        } else {
+        alert("Gracias por proporcionar tus datos.");
+        alert("¡Hola, " + nombre + " " + apellido + "! Bienvenido/a a Games Market.");
+        alert("A continuación te pediremos que vayas tomando nota de los productos que te pueden interesar para poder brindarte una experiencia grandiosa.");
+        alert("Recuerda que toda esta información es 100% confidencial y con objetivos de brindar la mejor asistencia para tu comodidad.");
+        alert("Recuerda también tomar nota de las compras que incluyas en el carrito para que puedas asegurar los gastos que estás por realizar. Muchas gracias por confiar en nosotros");
+            }
+        }
+    }
 }
+// Llamamos a la función para que muestre los mensajes y solicite los datos al cliente.
 saludar();
+
+
 
 //¿Eres mayor de edad?
 let edad = 20;
