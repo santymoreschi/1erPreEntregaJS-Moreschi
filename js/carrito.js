21a6e6c38e4ed360b0000115177989666311052c
santymoreschi <santycuarto@gmail.com>
2023-08-12 17:40:49 -0300

    Tercer PreEntrega

--- js/carrito.js
+++ js/carrito.js
@@ -1,44 +1,21 @@
-//menu responsive
-document.querySelector('.menu-icon').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('active');
-});
+const carritoContainer = document.getElementById("carritoContainer")
+let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 
+console.table(cartItems);
+const setCartItems = new Set(cartItems)
 
+console.table(setCartItems);
 
-/*
-//Saludo
-function saludar() {
-    alert("Sección de carrito");
-    alert("esperamos que hayas tomado nota de los articulos con sus precios para que puedas verificar tus gastos!! COMENCEMOS: ")
+if (cartItems.length > 0) {
+    carritoContainer.innerHTML = ''
+    cartItems.forEach(i =>{
+    carritoContainer.innerHTML +=`<img src="${i.image}" alt="${i.name}" style="height: 150px; object-fit: cover;">
+    <h3>${i.name}</h3>
+    <p>Categoría: ${i.category}</p>
+    <p>Cantidad: ${i.quantity}</p>
+    <p>Precio: $${i.price * i.quantity}</p>
+`
+})
+}else{
+    carritoContainer.innerHTML = `<h1>Vaya, parece que todavia no has agregado productos al carrito</h1>`
 }
-
-//Acumulador de productos
-let resultado = 0;
-let ticket = "Detalle de la Compra:";
-let rta = "";
-
-do {
-let producto = prompt("ingrese el nombre del producto:");
-let precio = Number(prompt("ingrese el valor del producto: "))
-
-
-if (producto && precio && !isNaN(precio)) {
-resultado += precio;
-ticket += "\n" + producto + "\t$" + precio.toFixed(2);
-rta = prompt("¿Desea salir? (Ingrese 'SI' para salir.)").toUpperCase();
-} else {
-alert("Ingrese valores válidos para el nombre y el precio del producto.");
-}
-} while (rta !== "SI");
-alert(ticket + "\nTotal: $" + resultado.toFixed(2));
-
-
-
-//Saludo
-function saludar() {
-alert("Bienvenido a Games Market");
-alert("A continuación te pediremos que ingreses algunos datos para poder darte una experiencia grandiosa");
-alert("Recuerda que toda esta informacion es 100% confidencial y con objetivos de brindar la mejor asistencia para su comodidad");
-alert("Recuerda tambien tomar nota de las compras que incluyas en el carrito para que puedas asegurar los gastos que estas por realizar");
-}
-saludar();*/
--- js/productos.js
+++ js/productos.js
@@ -28,17 +28,17 @@ openModalButtons.forEach((button) => {
 
 
 
-const openModalButton = document.getElementById('openModal');
-const closeModalButton = document.getElementById('closeModalBtn');
-const modal = document.getElementById('myModal');
+// const openModalButton = document.getElementById('openModal');
+// const closeModalButton = document.getElementById('closeModalBtn');
+// const modal = document.getElementById('myModal');
 
-openModalButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+// openModalButton.addEventListener('click', () => {
+//     modal.style.display = 'block';
+// });
 
-closeModalButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+// closeModalButton.addEventListener('click', () => {
+//     modal.style.display = 'none';
+// });
 
 function loadCartFromLocalStorage() {
     const cartItemsString = localStorage.getItem('cartItems');
@@ -48,14 +48,27 @@ function loadCartFromLocalStorage() {
 }
 
 //Productos en el carrito
-let cartItems = [];
+let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
 function addToCart(event) {
     const productId = parseInt(event.target.getAttribute('data-product-id'));
+
     const productToAdd = products.find((product) => product.id === productId);
+
     if (productToAdd) {
-        cartItems.push(productToAdd);
+     
         console.log(`Producto "${productToAdd.name}" añadido al carrito.`);
-        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        const productFound = cartItems.find(i => i.id == productToAdd.id)
+
+        if (productFound) {
+            const productFoundIndex = cartItems.findIndex(i => i.id == productToAdd.id)
+            cartItems[productFoundIndex].quantity ++
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        }else{
+            cartItems.push(productToAdd)
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        }
+
         event.target.classList.add('added-to-cart');
         setTimeout(() => {
             event.target.classList.remove('added-to-cart');
@@ -70,16 +83,16 @@ function addToCart(event) {
 
 //Productos disponibles en la tienda del index
 const products = [
-    { id: 1, name: 'Smartphone', category: 'Electrónicos', price: 300, image: '../img/smartphone.jpg' },
-    { id: 2, name: 'Smart TV', category: 'Electrónicos', price: 800, image: '../img/smart.jpg' },
-    { id: 3, name: 'Laptop', category: 'Electrónicos', price: 1200, image: '../img/laptop.jpg' },
-    { id: 4, name: 'Tablet', category: 'Electrónicos', price: 250, image: '../img/tablet.jpg' },
-    { id: 5, name: 'Auriculares', category: 'Electrónicos', price: 250, image: '../img/auriculares.jpg' },
-    { id: 6, name: 'Smart tv samsung', category: 'Electrónicos', price: 750, image: '../img/smartsamsung.jpg' },
-    { id: 7, name: 'Cascos VR ps5', category: 'Electrónicos', price: 1250, image: '../img/vr.jpg' },
-    { id: 8, name: 'Smartwaches', category: 'Electrónicos', price: 750, image: 'https://http2.mlstatic.com/D_NQ_NP_771823-MLA45810691826_052021-O.webp' },
-    { id: 9, name: 'Tablet Samsung Tab A7', category: 'Electrónicos', price: 538, image: 'https://http2.mlstatic.com/D_NQ_NP_639004-MLU69482268920_052023-O.webp' },
-    { id: 10, name: 'Mando ps4', category: 'Electrónicos', price: 290, image: 'https://http2.mlstatic.com/D_NQ_NP_686753-MLU69972650035_062023-O.webp' },
+    { id: 1, name: 'Smartphone', category: 'Electrónicos', price: 300, image: '../img/smartphone.jpg', quantity: 1 },
+    { id: 2, name: 'Smart TV', category: 'Electrónicos', price: 800, image: '../img/smart.jpg',quantity: 1 },
+    { id: 3, name: 'Laptop', category: 'Electrónicos', price: 1200, image: '../img/laptop.jpg',quantity: 1 },
+    { id: 4, name: 'Tablet', category: 'Electrónicos', price: 250, image: '../img/tablet.jpg',quantity:1 },
+    { id: 5, name: 'Auriculares', category: 'Electrónicos', price: 250, image: '../img/auriculares.jpg',quantity: 1},
+    { id: 6, name: 'Smart tv samsung', category: 'Electrónicos', price: 750, image: '../img/smartsamsung.jpg',quantity: 1 },
+    { id: 7, name: 'Cascos VR ps5', category: 'Electrónicos', price: 1250, image: '../img/vr.jpg',quantity: 1 },
+    { id: 8, name: 'Smartwaches', category: 'Electrónicos', price: 750, image: 'https://http2.mlstatic.com/D_NQ_NP_771823-MLA45810691826_052021-O.webp',quantity: 0 },
+    { id: 9, name: 'Tablet Samsung Tab A7', category: 'Electrónicos', price: 538, image: 'https://http2.mlstatic.com/D_NQ_NP_639004-MLU69482268920_052023-O.webp',quantity: 0 },
+    { id: 10, name: 'Mando ps4', category: 'Electrónicos', price: 290, image: 'https://http2.mlstatic.com/D_NQ_NP_686753-MLU69972650035_062023-O.webp',quantity: 0 },
 ];
 let filteredProducts = products.slice();
 
@@ -92,16 +105,14 @@ function showProducts() {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
         const isProductInCart = cartItems.some((item) => item.id === product.id);
-        const addButtonClass = isProductInCart ? 'add-to-cart-button disabled' : 'add-to-cart-button';
+        // const addButtonClass = isProductInCart ? 'add-to-cart-button disabled' : 'add-to-cart-button';
 
         productCard.innerHTML = `
             <img src="${product.image}" alt="${product.name}" style="height: 150px; object-fit: cover;">
             <h3>${product.name}</h3>
             <p>Categoría: ${product.category}</p>
             <p>Precio: $${product.price}</p>
-            <button class="${addButtonClass}" data-product-id="${product.id}">Añadir al carrito</button>
-            <button id="openModal" class="open-modal-button" data-product-id="${product.id}">Comprar</button>
-        
+            <button class="add-to-cart-button btn btn-primary" data-product-id="${product.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">Añadir al carrito</button>
         `;
         const openModal = document.getElementById('openModal');
         
@@ -110,6 +121,7 @@ function showProducts() {
 
     //"Añadir al carrito"
     const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
+
     addToCartButtons.forEach((button) => {
         button.addEventListener('click', addToCart);
     });
@@ -143,7 +155,6 @@ function openModal(product) {
         modal.style.display = 'none';
     S});
     }
-
 
 //Buscador de Productos por nombre
 function filterProducts() {
